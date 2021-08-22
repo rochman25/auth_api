22c7ea21aed80d0b8fa5b67e581aca49ae64e1f8
Zaenurrochman <zaenur.rochman98@gmail.com>
2021-08-22 09:35:31 +0700

    add encryption helper and AddUser use case

---
+++ src/Applications/security/EncryptionHelper.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+class EncryptionHelper {
+	async encryptPassword(password) {
+		throw new Error('ENCRYPTION_HELPER.METHOD_NOT_IMPLEMENTED');
+	}
+}
+
+module.exports = EncryptionHelper;
---
+++ src/Applications/security/_test/EncryptionHelper.test.js
@@ -0,0 +1,9 @@
+const EncryptionHelper = require('../EncryptionHelper');
+
+describe('EncryptionHelper interface', () => {
+	it('should throw error when invoke abstract behavior', async () => {
+		const encryptionHelper = new EncryptionHelper();
+
+		await expect(encryptionHelper.encryptPassword('dummy_password')).rejects.toThrowError('ENCRYPTION_HELPER.METHOD_NOT_IMPLEMENTED');
+	});
+});
